99957b7f6bff85771922591893317f06fb6e0b5a
antgoil78 <andreas@openresult.se>
2025-10-06 14:40:12 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Starting Prefect Orion server..."
 # Run Orion in foreground, bind to 0.0.0.0 so Snowflake can route traffic
-prefect orion start --host 0.0.0.0 --port 4200 &
+prefect server start --host 0.0.0.0 --port 4200 &
 
 sleep 5
 
