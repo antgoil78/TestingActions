719a45c2736403dc38acdec681d90fdc3e75914c
antgoil78 <andreas@openresult.se>
2025-10-07 08:57:26 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "Starting Prefect 3 Server..."
-prefect server start --host 127.0.0.1 --port 4200
+echo "Starting Prefect Orion server..."
+
+# Start Orion with correct host and external API URL
+prefect server start \
+  --host 0.0.0.0 \
+  --port 4200 \
+  --expose-api "https://abztue-vymkbmw-ov24823.snowflakecomputing.app:4200" &
+sleep 10
+
+echo "Starting Prefect worker..."
+prefect worker start --pool "snowflake"
