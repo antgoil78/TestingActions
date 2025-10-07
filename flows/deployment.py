706623deed89acd127f38b3d912cdd5bfc7f8735
antgoil78 <andreas@openresult.se>
2025-10-07 16:52:01 +0200

    Update deployment.py

--- flows/deployment.py
+++ flows/deployment.py
@@ -1,9 +1,7 @@
-from prefect.deployments import Deployment
-from dbt_sdl_mcc_ref_flow import dbt_sdl_mcc_ref_flow
+from flows.dbt_sdl_mcc_ref_flow import dbt_sdl_mcc_ref_flow
 
-# Build a deployment so the flow shows in the UI
-Deployment.build_from_flow(
-    flow=dbt_sdl_mcc_ref_flow,
+# Deploy programmatically (register with Prefect server)
+dbt_sdl_mcc_ref_flow.deploy(
     name="SDL_MCC_REF Deployment",
     work_queue_name="snowflake"
 )
