03c7ae05c4e624b07e23e1c9896dee511722b3d3
antgoil78 <andreas@openresult.se>
2025-10-07 13:43:55 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -e
 
-# -----------------------------------------------
-# IMPORTANT: Set this to your Snowflake service endpoint
-# Example: ifztue-vymkbmw-ov24823.snowflakecomputing.app
-# -----------------------------------------------
-export PREFECT_API_URL="https://ifztue-vymkbmw-ov24823.snowflakecomputing.app/api"
-export PREFECT_TELEMETRY_ENABLED=false
-
 echo "Starting Prefect Worker..."
 prefect worker start --pool "snowflake"
