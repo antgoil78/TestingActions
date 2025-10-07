8bdd854753dcc1c3e6721fa5d43bf97c6c3a21ea
antgoil78 <andreas@openresult.se>
2025-10-07 13:38:41 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,5 @@ set -e
 export PREFECT_API_URL="https://ifztue-vymkbmw-ov24823.snowflakecomputing.app/api"
 export PREFECT_TELEMETRY_ENABLED=false
 
-# Start Prefect server in background
-echo "Starting Prefect Server..."
-prefect server start --host 0.0.0.0 --port 4200 &
-SERVER_PID=$!
-
-# Give the server some time to initialize
-sleep 15
-
-# Start Prefect worker attached to 'snowflake' pool
 echo "Starting Prefect Worker..."
 prefect worker start --pool "snowflake"
-
-# Wait for server process (optional)
-wait $SERVER_PID
