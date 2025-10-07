143b7947e85cca31af1b2afd6b42ce2a7c2e4deb
antgoil78 <andreas@openresult.se>
2025-10-07 10:05:42 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "Starting Prefect server..."
-
-# Bind to all interfaces so Snowflake can proxy traffic
 prefect server start --host 0.0.0.0 --port 4200 &
-sleep 10
+sleep 15
+
+export PREFECT_API_URL="http://127.0.0.1:4200/api"
 
 echo "Starting Prefect worker..."
 prefect worker start --pool "snowflake"
