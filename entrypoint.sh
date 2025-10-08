bbee9089b4e2e39b4ffe538edf2b5e9f44505d99
antgoil78 <andreas@openresult.se>
2025-10-08 07:50:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ done
 
 # Register flows (build deployments)
 echo "Deploy flows..."
-prefect deploy -n "SDL_MCC_REF Deployment" flows/dbt_flow_sdl_mcc_ref.py:dbt_flow_sdl_mcc_ref.py
+prefect deploy -n "SDL_MCC_REF Deployment" flows/dbt_flow_sdl_mcc_ref.py:dbt_sdl_mcc_ref_flow
 
 echo "Starting Prefect Worker..."
 prefect worker start --pool "snowflake"
