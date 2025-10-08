c8f470dae2349161fce3aeebe5c7a8505d1e7f80
antgoil78 <andreas@openresult.se>
2025-10-08 08:43:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,29 +11,27 @@ SERVER_PID=$!
 
 echo "Waiting for Prefect API to become available..."
 for i in {1..30}; do
-    if curl -s http://127.0.0.1:4200/api > /dev/null; then
+    if curl -s $PREFECT_API_URL > /dev/null; then
         echo "Prefect API is up!"
         break
     fi
     sleep 2
 done
 
-prefect work-pool create --type process snowflake
+# Create work-pool (ignore if exists)
+prefect work-pool create --type process snowflake || true
 
-
-# Register flows (build deployments)
-#echo "Deploy flows..."
-#prefect deploy -n "SDL_MCC_REF Deployment" flows/dbt_flow_sdl_mcc_ref.py:dbt_sdl_mcc_ref_flow --name dbt_sdl_mcc_ref --pool "snowflake"
-#prefect deploy --entrypoint flows/dbt_sdl_mcc_ref_flow.py:dbt_sdl_mcc_ref_flow --name "SDL_MCC_REF Deployment" --pool "snowflake"
-echo "Deploying flow..."
-prefect deploy flows/dbt_flow_sdl_mcc_ref.py:dbt_sdl_mcc_ref_flow --name "SDL_MCC_REF Deployment" --pool "snowflake"
-
-
-#echo "Deploying flow..."
-#prefect deploy flows/dbt_flow_sdl_mcc_ref.py:dbt_sdl_mcc_ref_flow --name "SDL_MCC_REF Deployment" --pool "snowflake"
-
+# Deploy flow (ignore if already deployed)
+prefect deploy flows/dbt_flow_sdl_mcc_ref.py:dbt_sdl_mcc_ref_flow --name "SDL_MCC_REF Deployment" --pool "snowflake" || true
 
 echo "Starting Prefect Worker..."
-prefect worker start --pool "snowflake"
+prefect worker start --pool "snowflake" &
+
+# Give the worker a few seconds to start
+sleep 5
+
+# Trigger the flow run automatically
+echo "Triggering first flow run..."
+prefect deployment run "SDL_MCC_REF Deployment"
 
 wait $SERVER_PID
